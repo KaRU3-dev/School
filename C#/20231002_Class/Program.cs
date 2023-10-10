f63d5d9341209ca4dae918a01bf5cf690fd2061b
KaRU3 <121723731+KaRU3-dev@users.noreply.github.com>
2023-10-10 12:12:23 +0900

    Start 3d game class

---
+++ C#/20231002_Class/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace _20231002_Class
+{
+    public class Person{
+
+        private string _name;
+        private int _age;
+
+        public string Name{
+            get { return _name; }
+            set {
+                if (!string.IsNullOrEmpty(value)){
+                    _name = value;
+                }
+                else {
+                    throw new ArgumentException("Name cannot be null or empty");
+                }
+            }
+        }
+
+        public int Age {
+            get { return _age; }
+            set {
+                if (value >= 0 && value <= 120){
+                    _age = value;
+                }
+                else {
+                    throw new ArgumentException("Age is invalid number");
+                }
+            }
+        }
+
+        // Constructor
+        public Person(string name, int age){
+            Name = name;
+            Age = age;
+        }
+
+        // Method
+        public void DisplayInfo(){
+            Console.WriteLine($"Name: {Name}, Age: {Age}");
+        }
+    }
+
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Please enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Person person = new Person(name, age);
+
+            person.DisplayInfo();
+        }
+    }
+}
---
+++ C#/20231002_Prac01_Okumura/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace _20231002_Prac01_Okumura{
+    internal class Program{
+        public static void Main(string[] args){
+
+            Console.Write("Please enter your balance: ");
+            int balance = int.Parse(Console.ReadLine());
+            Console.Write("Please enter your account number: ");
+            int accountNumber = int.Parse(Console.ReadLine());
+
+            BankAccount bankAccount = new BankAccount(balance, accountNumber);
+
+            bankAccount.GetBallance();
+
+            Console.Write("Please enter your deposit amount: ");
+            int depositAmount = int.Parse(Console.ReadLine());
+            bankAccount.Deposit(depositAmount);
+
+            bankAccount.GetBallance();
+
+            Console.Write("Please enter your withdraw amount: ");
+            int withdrawAmount = int.Parse(Console.ReadLine());
+            bankAccount.Withdraw(withdrawAmount);
+
+            bankAccount.GetBallance();
+        }
+    }
+
+
+    public class BankAccount{
+        private int _balance;
+        private int _accountNumber;
+
+        public int Balance{
+            get { return _balance; }
+            set {
+                if (value > 0){
+                    _balance = value;
+                }
+                else {
+                    throw new ArgumentException("Balance cannot be null or empty");
+                }
+            }
+        }
+
+        public int AccountNumber{
+            get { return _accountNumber; }
+            set {
+                if (value > 0){
+                    _accountNumber = value;
+                }
+                else {
+                    throw new ArgumentException("AccountNumber cannot be null or empty");
+                }
+            }
+        }
+
+        // Constructor
+        public BankAccount(int balance, int accountNumber){
+            Balance = balance;
+            AccountNumber = accountNumber;
+        }
+
+        // Method
+        public void GetBallance(){
+            Console.WriteLine($"Balance: {Balance}");
+        }
+        public void Deposit(int amount){
+            Balance += amount;
+        }
+        public void Withdraw(int amount){
+            Balance -= amount;
+        }
+    }
+}

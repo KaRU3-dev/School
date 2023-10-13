2d23d7101dc9488ffbef1115a15fd0eed4e81d7c
KaRU3 <121723731+KaRU3-dev@users.noreply.github.com>
2023-10-13 11:38:31 +0900

    Java study

---
+++ JavaStudy/src/main/java/school/Main.java
@@ -0,0 +1,17 @@
+package school;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // This is a comment.
+        System.out.println("I love pizza!!");
+        System.out.println("It's really good.");
+
+
+        /*
+            This is a multi line comment.
+         */
+        System.out.print("\tHi, I'm \"Mike\"!\n");
+        System.out.print("\tI like pizza!!\\\\\\\n");
+    }
+}
---
+++ JavaStudy/src/main/java/school/Part2.java
@@ -0,0 +1,24 @@
+package school;
+
+/**
+ * This class is Java study part2
+ * This contains variables
+ * now: <a href="https://youtu.be/xk4_1vDrzzo?t=1420">動画ココやねん</a>
+ */
+public class Part2 {
+
+    // Variables
+                                       // SIZE       primitive/reference       value
+    boolean bool = true;               // 1 bit      primitive                 true or false
+    byte aByte = 127;                  // 1 byte     primitive                 -128 ~ 127
+    short aShort = 32767;              // 2 bytes    primitive                 -32768 ~ 32767
+    int anInt = 2000000000;            // 4 bytes    primitive                 -2 billion ~ 2 billion
+    long aLong = 9000000000000000000L; // 8 bytes    primitive                 -9 quintillion ~ 9 quintillion
+
+    float aFloat = 3.141592f;          // 4 bytes    primitive                 fractional number up to 6~7 digits
+    double aDouble = 3.121592653589793;// 8 bytes    primitive                 fractional number up to 15 digits
+
+    //char aChar = "A";                // 2 bytes    primitive                 a single character/letter/ASCII value
+    String aString = "Hello";          // varies     reference                 a sequence of characters
+
+}
